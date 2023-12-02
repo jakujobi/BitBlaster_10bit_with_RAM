965f1c34673210d08e3fbce7795562c8bb85455b
jakujobi <akujobi365@gmail.com>
2023-12-02 17:45:59 -0600

    Changed Variables, and added tri state to the data_out
    This is to make sure the data-out from the RAM does not interrupt the other components when writing to the bus

--- SV Module Files/ram_1024x10.sv
+++ SV Module Files/ram_1024x10.sv
@@ -1,24 +1,33 @@
 module ram_1024x10 (
     input logic clk,
-    input logic write_enable,
+    input logic EN_write_to_RAM,    // Enable signal to write to RAM
+    input logic EN_read_from_RAM,   // Enable signal to read from RAM
     input logic[9:0] address, // 10-bit address for 1024 locations
     input logic[9:0] data_in,  // 10-bit data input
+
     output logic[9:0] data_out // 10-bit data output
 );
 
-    // 1024x10 bit memory array
-    logic[9:0] memory_array[1023:0];
+// 1024x10 bit memory array
+logic[9:0] memory_array[1023:0];
 
-    // Load into the RAM memory
-    always_ff @(negedge clk) begin
-            if (write_enable) begin
-                // Write operation
-                memory_array[address] <= data_in;
-            end
+// Load into the RAM memory
+always_ff @(negedge clk) begin
+        if (EN_write_to_RAM) begin
+            // Write operation
+            memory_array[address] <= data_in;
+        end
+end
+
+// Read from RAM operation
+//assign data_out = memory_array[address];
+always_comb begin
+    if (EN_read_from_RAM) begin
+        data_out = memory_array[address];
+    end else begin
+        data_out = 10'bz;             // High-impedance state if not enabled
     end
-
-    // Read operation
-    assign data_out = memory_array[address];
+end
 
 endmodule
 
