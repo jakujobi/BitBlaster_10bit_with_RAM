07b45f9cdd5749b8874bbdc046b739229aa4229f
jakujobi <akujobi365@gmail.com>
2023-12-02 23:34:58 -0600

    Update ram_1024x10.sv

--- SV Module Files/ram_1024x10.sv
+++ SV Module Files/ram_1024x10.sv
@@ -42,28 +42,3 @@ always_comb begin
 end
 
 endmodule
-
-/*
-Inputs:
-
-clk: The clock signal for synchronizing the read/write operations.
-reset: An asynchronous reset to clear the entire RAM.
-write_enable: A signal to enable writing to the memory. When high, the module performs a write operation.
-address: A 10-bit address to specify which of the 1024 locations to access.
-data_in: The 10-bit data to be written to the specified address.
-Output:
-
-data_out: The 10-bit data read from the specified address.
-Memory Array:
-
-A 1024x10 bit memory array is declared to store the data.
-Write Operation:
-
-On the rising edge of the clock, if write_enable is high and reset is low, the data_in is written to the location specified by address.
-Read Operation:
-
-The data at the location specified by address is continuously assigned to data_out.
-Reset Operation:
-
-On the rising edge of the clock, if reset is high, the entire memory array is cleared (set to 0).
-*/
